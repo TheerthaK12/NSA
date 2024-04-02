eca14bb352a00bc4595da093c07af2fa4acd23d2
TheerthaK12 <152851785+TheerthaK12@users.noreply.github.com>
2024-04-02 15:14:13 +0530

    Add files via upload

---
+++ name.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+echo "enter the name:"
+read name
+echo "enter Roll number:"
+read roll
+echo "enter the mark:"
+read mark
+echo "........"
+echo "DETAILS"
+echo "name is : $name"
+echo "Roll number is:$roll"
+echo "Mark is:$mark"
+
